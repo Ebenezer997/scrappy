fe8f3a92462cc4091635c3ff4bc9d133e233df44
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2022-02-14 20:56:34 -0500

    Add files via upload

---
+++ finspy.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class FinspySpider(scrapy.Spider):
+    name = 'finspy'
+    allowed_domains = ['politico.com']
+    start_urls = ['https://www.politico.com/search?q=technology']
+
+    def parse(self, response):
+        for articles in response.css('article.story-frag.format-ml'):
+            yield {
+                'Title' : articles.css('img').attrib['alt'],
+                'Link' : articles.css('a').attrib['href'],
+                'Intro': articles.css('div.tease').css('p::text').get(),
+                'Category': articles.css('p.category::text').get(),
+                'DateandTime': articles.css('time').attrib['datetime'],
+            }
+        
+        for button in response.css('a.button'):
+            if button.css('::text').get() == 'Next page »':
+                next_page = button.attrib['href']
+                print("\n Next Page :" + next_page + "\n")
+                yield response.follow(next_page, callback=self.parse)
+        
